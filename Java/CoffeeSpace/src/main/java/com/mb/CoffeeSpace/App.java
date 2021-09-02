906cb42e4a2de5d160dec3f8923cf356795ca2fa
Maria Borodina <linda.en.casa@gmail.com>
2021-09-02 20:39:05 +0300

    CoffeeSpace app. Beginning with Spring.

--- Java/CoffeeSpace/src/main/java/com/mb/CoffeeSpace/App.java
+++ Java/CoffeeSpace/src/main/java/com/mb/CoffeeSpace/App.java
@@ -1,13 +1,30 @@
 package com.mb.CoffeeSpace;
 
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     static ConsoleLogger logger = new ConsoleLogger();
 
+    public App(ConsoleLogger consoleLogger)
+    {
+        this.logger = consoleLogger;
+    }
+
     public static void main(String[] args)
     {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+
+        ConsoleLogger localLogger = (ConsoleLogger)ctx.getBean("eventLogger");
+        localLogger.log("test spring message");
+
+
         CoffeeKind kinda1 = new CoffeeKind("First");
 
         logger.log(kinda1.getName() + ": id = " + kinda1.getId());
 
     }
+
+
 }
