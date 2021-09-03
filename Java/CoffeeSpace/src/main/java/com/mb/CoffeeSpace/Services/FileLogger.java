14cc18ea4a0a7969a86e41d412e354fff7ee3a8e
Maria Borodina <linda.en.casa@gmail.com>
2021-09-03 13:55:31 +0300

    added init to FileLogger

--- Java/CoffeeSpace/src/main/java/com/mb/CoffeeSpace/Services/FileLogger.java
+++ Java/CoffeeSpace/src/main/java/com/mb/CoffeeSpace/Services/FileLogger.java
@@ -3,6 +3,7 @@ package com.mb.CoffeeSpace.Services;
 import com.mb.CoffeeSpace.Intefaces.EventLogger;
 import com.mb.CoffeeSpace.Models.Event;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +14,15 @@ public class FileLogger implements EventLogger {
     public FileLogger(String filename) {
         this.filename = filename;
     }
+
+    public void init() throws IOException {
+        var file = new File(filename);
+        if(! file.canWrite())
+        {
+            throw new IOException(String.format("Cannot write into file %s.", filename));
+        }
+    }
+
 
     @Override
     public void log(String message) throws IOException {
